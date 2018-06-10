cd98bba5ed40c53bc42e2a1d6d3b2b11b834c6ae
lin <1875817816@qq.com>
2018-06-10 09:35:36 +0800

    第二次提交

---
+++ src/main/java/Product.java
@@ -0,0 +1,5 @@
+/**
+ * Created by lenovo on 2018/6/10.
+ */
+public class Product {
+}
---
+++ src/main/java/User.java
@@ -0,0 +1,44 @@
+import java.io.Serializable;
+
+/**
+ * Created by lenovo on 2018/6/8.
+ */
+public class User implements Serializable {
+
+    private String id;
+    private String name;
+    private String password;
+
+    public User() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public User(String id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+}
