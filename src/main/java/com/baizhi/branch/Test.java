4846384e7d9dada0f184a14bed2e572cde21436a
lin <1875817816@qq.com>
2018-06-10 17:43:42 +0800

    分支第一次提交

---
+++ src/main/java/com/baizhi/branch/Test.java
@@ -0,0 +1,11 @@
+package com.baizhi.branch;
+
+/**
+ * Created by lenovo on 2018/6/10.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("我是分支");
+    }
+}
